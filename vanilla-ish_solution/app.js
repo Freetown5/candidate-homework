44cb31154be709a17d77ef22694f8d130b1ece5a
Isa Conteh <iconteh21@gmail.com>
2018-10-24 13:38:39 -0400

    attempted to access json from endpoint via axos, also further refined UI

---
+++ vanilla-ish_solution/app.js
@@ -0,0 +1,19 @@
+console.log("Working?");
+
+// axios.get('https://s3.amazonaws.com/api-fun/books.json')
+//     .then(function(response){
+//         console.log(response);
+//     })
+//     .catch(function(error){
+//         console.log(error)
+//     })
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET', 'https://s3.amazonaws.com/api-fun/books.json', true);
+
+// request.onload = function () {
+
+// }
+
+// request.send();
